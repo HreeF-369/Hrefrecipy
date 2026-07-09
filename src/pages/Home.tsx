8057b8e386e3d3a7620159e539ff5d89355d2d2b
HreeF-369 <achrafalhyan868@gmail.com>
2026-07-09 12:04:20 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -172,6 +172,7 @@ export default function Home() {
   const handleOpenRecipe = (recipe: Recipe) => {
     setSelectedRecipe(recipe);
     setIsModalOpen(true);
+    window.history.pushState({ recipeId: recipe.id }, '', `/recipe/${recipe.id}`);
   };
 
   const categories = [
@@ -482,6 +483,7 @@ export default function Home() {
         isOpen={isModalOpen} 
         onClose={() => {
           setIsModalOpen(false);
+          window.history.pushState(null, '', `/`);
         }} 
       />
 
